4221448c8fbd98e59e1cea78f26ce0201463936d
Von-Thor <wator1uk@gmail.com>
2014-09-29 08:56:44 +0200

    Refactoring

--- Game_of_Life/src/game/Cell.java
+++ Game_of_Life/src/game/Cell.java
@@ -1,45 +1,58 @@
 
 package game;
  
-public class Cell implements Cloneable {
+public class Cell implements Cloneable 
+{
  
-    private boolean alive;
- 
-    public Cell(boolean alive) {
-        this.setAlive(alive);
-    }
- 
-    public boolean isAlive() {
-        return alive;
-    }
- 
-    public void setAlive(boolean alive) {
-        this.alive = alive;
-    }
- 
-    @Override
-    public Cell clone() {
-        return new Cell(alive);
-    }
- 
-    public void changeState(int neighboursCount) {
- 
-        if (alive) {
- 
-            if (neighboursCount < 2) {
-                alive = false;
-            } else if (neighboursCount > 3) {
-                alive = false;
-            }
- 
-        } else {
- 
-            if (neighboursCount == 3) {
-                alive = true;
-            }
- 
-        }
- 
-    }
- 
+	private boolean alive;
+
+	public Cell(boolean alive) 
+	{
+		this.setAlive(alive);
+	}
+
+	public boolean isAlive() 
+	{
+		return alive;
+	}
+
+	public void setAlive(boolean alive) 
+	{
+		this.alive = alive;
+	}
+
+	@Override
+	public Cell clone() 
+	{
+		return new Cell(alive);
+	}
+
+	public void changeState(int neighboursCount) 
+	{
+
+	if (alive) 
+	{
+
+		if (neighboursCount < 2) 
+		{
+			alive = false;
+		} 
+		else if (neighboursCount > 3) 
+		{
+			alive = false;
+		}
+
+	} 
+	else 
+	{
+
+		if (neighboursCount == 3) 
+		{
+			alive = true;
+		}
+
+	}
+
+	}
+
 }
